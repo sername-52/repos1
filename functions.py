e5accb05e52cded956c25a5f4d3f757d425eff74
sername-52 <spirinkr1708@gmail.com>
2026-06-01 21:21:46 +0500

    Update functions.py

--- functions.py
+++ functions.py
@@ -1,9 +1,3 @@
-from math import sqrt
-
-def plos_square(a, b):
-    plos = a * b
-    print(f"Площадь прямоугольника равна {plos}")
-
 def plos_round(r):
     plosr = 3.14 * r ** 2
     print(f"Площадь круга равна {plosr}")  
