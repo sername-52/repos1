8e1f5be8c42bfc12cf27e6cadcd663b925de700a
sername-52 <spirinkr1708@gmail.com>
2026-06-01 21:02:07 +0500

    plostriangle

--- functions.py
+++ functions.py
@@ -1,3 +1,5 @@
+from math import sqrt
+
 def plos_square(a, b):
     plos = a * b
     print(f"Площадь прямоугольника равна {plos}")
@@ -5,3 +7,9 @@ def plos_square(a, b):
 def plos_round(r):
     plosr = 3.14 * r ** 2
     print(f"Площадь круга равна {plosr}")  
+
+def plos_triangle(a, b, c):
+    import math
+    p = (a + b + c) / 2
+    plost = sqrt((a - p) * (b - p) * (c - p))
+    print(f"Площадь треугольника равна {plost}")
