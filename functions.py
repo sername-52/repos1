3e711473da4b692caaa2cb6d2d0bb0bc5e4a3d2d
sername-52 <spirinkr1708@gmail.com>
2026-06-01 20:55:06 +0500

    plosround

--- functions.py
+++ functions.py
@@ -2,3 +2,6 @@ def plos_square(a, b):
     plos = a * b
     print(f"Площадь прямоугольника равна {plos}")
 
+def plos_round(r):
+    plosr = 3.14 * r ** 2
+    print(f"Площадь круга равна {plosr}")  
